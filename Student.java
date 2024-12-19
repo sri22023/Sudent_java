36e0cb91c43608d79279552435b8f14707f13283
Milky19 <97377003+Milky19@users.noreply.github.com>
2024-12-19 13:10:05 +0545

    new-code

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    public static void main(String[] args) {
+        int rollno = 1596;
+        String sname = "KRISHNA";
+        String course = "PYTHON"; 
+        System.out.println("LEARN HERE AND LEAD ANYWHERE!!!);
+        System.out.println("My Name is: " + sname);
+        System.out.println("My Course is: " + course);
+        System.out.println("My Roll Number is: " + rollno);
+        System.out.println("THANK YOU");
+    }
+}
