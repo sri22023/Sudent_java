3ab80ae70d59f6516cca66d1087a6b470c857e17
Milky19 <97377003+Milky19@users.noreply.github.com>
2025-03-14 20:15:17 +0530

    Updated

--- Student.java
+++ Student.java
@@ -7,6 +7,7 @@ public class Student {
         System.out.println("My Name is: " + sname);
         System.out.println("My Course is: " + course);
         System.out.println("My Roll Number is: " + rollno);
+        System.out.println("=======================");
         System.out.println("THANK YOU");
     }
 }
