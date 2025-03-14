7a0bf47914e5faa91f8ab457e0463f64b0400289
Milky19 <97377003+Milky19@users.noreply.github.com>
2025-03-14 20:30:06 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -9,5 +9,9 @@ public class Student {
         System.out.println("My Roll Number is: " + rollno);
         System.out.println("=======================");
         System.out.println("THANK YOU");
+         System.out.println("THANK YOU");
+         System.out.println("THANK YOU");
+         System.out.println("THANK YOU");
+         System.out.println("THANK YOU");
     }
 }
